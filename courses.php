397417d7978816c9bed13e458fcbe67ba115b62e
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-10-01 22:38:29 -0500

    Create courses.php

---
+++ courses.php
@@ -0,0 +1,9 @@
+<?php
+required_once("util.db.php");
+require_once("model-courses.php");
+$pageTitle = "Courses";
+include "view-header.php";
+$courses = selectCourses();
+include "view-courses.php";
+include "view-footer.php";
+?>
