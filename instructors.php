d7b5996f372c3e53cfe4ff2ad3229a73704e00ae
dianachuerta <157341983+dianachuerta@users.noreply.github.com>
2024-10-01 21:58:35 -0500

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -1,1 +1,9 @@
-
+<?php
+required_once("util.db.php");
+require_once("model-instructors.php");
+$pageTitle = "Instructors";
+include "view-header.php";
+$instructors = selectInstructors();
+include "view-instructors.php";
+include "view-footer.php";
+?>
